7a97200a3ded0b30156a388d58be82cbf97d1cb4
geekedup7 <david.dominguez2@my.utsa.edu>
2025-04-03 13:09:35 -0500

    Update Driver.java

--- assignment3G10/src/Driver.java
+++ assignment3G10/src/Driver.java
@@ -1,7 +1,24 @@
 import java.io.IOException;
+import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) throws IOException {
+        Scanner scnr = new Scanner(System.in);
         Login loginSystem = new Login();
-        loginSystem.login();
+        int choice;
+        PatientManager patientManager = loginSystem.login(); //gets instance of patientManager
+
+        System.out.println("Welcome to the dashboard, choose an option");
+        System.out.println("1 - View Patient Info, 2 - View/Lookup Patient, 3 - Edit Patient Info");
+        choice = scnr.nextInt();
+
+        switch (choice) {
+            case 1 :
+            if (patientManager != null) {
+            patientManager.viewPatientInfo(patientManager.getUsername());
+            }
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 }
