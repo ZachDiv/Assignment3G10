c64f1b10b316b5db5faf8bfb74c9d9f467b5050c
geekedup7 <david.dominguez2@my.utsa.edu>
2025-04-04 02:07:04 -0500

    Update Driver.java

--- assignment3G10/src/Driver.java
+++ assignment3G10/src/Driver.java
@@ -1,24 +1,40 @@
 import java.io.IOException;
 import java.util.Scanner;
+
 public class Driver {
+
     public static void main(String[] args) throws IOException {
         Scanner scnr = new Scanner(System.in);
         Login loginSystem = new Login();
         int choice;
         PatientManager patientManager = loginSystem.login(); //gets instance of patientManager
 
-        System.out.println("Welcome to the dashboard, choose an option");
-        System.out.println("1 - View Patient Info, 2 - View/Lookup Patient, 3 - Edit Patient Info");
-        choice = scnr.nextInt();
+        System.out.println("Welcome to the dashboard, choose an option:");
+        while (true) {
+            System.out.println("\n1 - View Patient Info, 2 - View/Lookup Patient, 3 - Edit Patient Info, 4 - Exit");
+            choice = scnr.nextInt();
 
-        switch (choice) {
-            case 1 :
-            if (patientManager != null) {
-            patientManager.viewPatientInfo(patientManager.getUsername());
+            switch (choice) {
+                case 1:
+                    if (patientManager != null) {
+                        patientManager.viewPatientInfo(patientManager.getUsername());
+                    }
+                    break;
+
+                case 3:
+                    if (loginSystem.isPatient() && patientManager != null) {
+                        patientManager.editPatientInfo(patientManager.getUsername());
+                    }
+
+                    if(loginSystem.isStaff() && patientManager != null) {
+                        patientManager.editAnyPatientInfo();
+                    }
+                case 4:
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    throw new AssertionError();
             }
-                break;
-            default:
-                throw new AssertionError();
         }
     }
 }
